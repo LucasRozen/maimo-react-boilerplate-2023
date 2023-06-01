bbc4453c9bf667c23175180d47a08e3490bea8b5
lukirozen <lucaslrozenberg@gmail.com>
2023-06-01 11:11:56 -0300

    Anchors

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,31 @@ import Header from '../Header/Header';
 import Hero from '../Hero/Hero';
 import Footer from '../Footer/Footer';
 import CardsContainer from '../CardsContainer/CardsContainer';
+import Cta from '../Cta/Cta';
+import Form from '../Form/Form';
+import { Element } from 'react-scroll';
 
 const App = () => {
   return (
-    <div className='App'>
-      <Header />
-      <Hero />
-      <CardsContainer />
-      <Footer />
+    <div className='App'>     
+      <Element name="header" className="element">
+        <Header />
+      </Element>
+      <Element name="hero" className="element">
+        <Hero />
+      </Element>
+      <Element name="cardsContainer" className="element">
+        <CardsContainer />
+      </Element>
+      <Element name="form" className="element">
+        <Form />
+      </Element>
+      <Element name="cta" className="element">
+        <Cta />
+      </Element>
+      <Element name="footer" className="element">
+        <Footer />
+      </Element>
     </div>
   );
 };
